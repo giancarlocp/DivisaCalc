ea35f293a3a5d235a5963862dac315f5597289c4
Giancarlos <giancarlocp@yahoo.com>
2020-05-31 19:50:07 -0400

    adding devolution and close fields

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-navigator.serviceWorker&&navigator.serviceWorker.register("sw.js")
-let pr=1e5,fx=t=>Math.round(t*pr)/pr
-function G(){let t={c:{u:5,b:1e3,c:20},p:{u:0,b:0,c:0},v:{u:0,b:0,c:0},d:{u:0,b:0,c:0}}
-function u(u,c){"u"===c?(t[u].b=fx(t[u].u*u2b),t[u].c=fx(t[u].b/c2b)):"b"===c?(t[u].u=fx(t[u].b/u2b),t[u].c=fx(t[u].b/c2b)):"c"===c&&(t[u].b=fx(t[u].c*c2b),t[u].u=fx(t[u].b/u2b))}let c=()=>t.p.u*u2b+t.p.b+t.p.c*c2b
-function e(){t.v.b=fx(c()-t.c.b),t.v.u=fx(t.v.b/u2b),t.v.c=fx(t.v.b/c2b)}let b=inputfn(function(c){let{value:b,id:m}=c.target
-b=num(b)
-let[n,i]=m
-t[n][i]=b,"c"==n?(u(n,i),e()):"p"==n?e():"d"==n&&u(n,i)}),n=t=>m(".item",{style:styl(t)},t)
-return{view:({attrs:u})=>(u2b=u.s.u2b,c2b=u.s.c2b,m(".container",[m(".item.h","$"),m(".item.h","Bs"),m(".item.h","Co"),m(""),b(t.c.u,"cu"),b(t.c.b,"cb"),b(t.c.c,"cc"),m(".item.v","Fact"),b(t.p.u,"pu"),b(t.p.b,"pb"),b(t.p.c,"pc"),m(".item.v","Pago"),n(t.v.u),n(t.v.b),n(t.v.c),m(".item","Vuelto"),m(""),m(""),m(""),m("",{style:"color:teal"},"."),b(t.d.u,"du"),b(t.d.b,"db"),b(t.d.c,"dc")]))}}let styl=t=>t<0?"background:tomato":"",inputfn=t=>(u,c)=>m("input.item",{type:"number",style:styl(u),value:u,id:c,onchange:t}),num=t=>""==t?0:parseFloat(t)
-function AppView(){let t={u2b:200,c2b:50}
-function u(u){let{value:c,id:e}=u.target
-c=num(c),t[e]=c}let c=(t,c,e)=>m(".itex",[m("label",t),inputfn(u)(c,e)])
-return{view:u=>m("",[m(".flx",[c("Bs/$",t.u2b,"u2b"),c("Bs/Co",t.c2b,"c2b")]),m(G,{s:t})])}}m.mount(document.getElementById("app"),AppView)
+//navigator.serviceWorker&&navigator.serviceWorker.register("sw.js")
+const pr=1e5,fx=b=>Math.round(b*pr)/pr
+function G({attrs:b}){console.log("G",b)
+const c={f:{u:5,b:1e3,c:20},p:{u:0,b:0,c:0},v:{u:0,b:0,c:0},d:{u:0,b:0,c:0},c:{u:0,b:0,c:0},e:{u:0,b:0,c:0}}
+function u(b,u){"u"===u?(c[b].b=fx(c[b].u*u2b),c[b].c=fx(c[b].b/c2b)):"b"===u?(c[b].u=fx(c[b].b/u2b),c[b].c=fx(c[b].b/c2b)):"c"===u&&(c[b].b=fx(c[b].c*c2b),c[b].u=fx(c[b].b/u2b))}const t=b=>c[b].u*u2b+c[b].b+c[b].c*c2b
+function e(b){let[u,e,n]="d"==b?"vdc":"fpv"
+c[n].b=fx(t(e)-c[u].b),c[n].u=fx(c[n].b/u2b),c[n].c=fx(c[n].b/c2b)}const n=inputfn(function(b){let{value:t,id:n}=b.target
+t=num(t)
+const[l,m]=n
+c[l][m]=t,"f"==l?(u(l,m),e(l)):"p"==l||"d"==l?e(l):"e"==l&&u(l,m)}),l=b=>m(".item",{style:styl(b)},b)
+return{view:({attrs:b})=>(u2b=b.u2b,c2b=b.c2b,m(".container",[m("label","$"),m("label","Bs"),m("label","Co"),m(""),n(c.f.u,"fu"),n(c.f.b,"fb"),n(c.f.c,"fc"),m("label.v","Fact"),n(c.p.u,"pu"),n(c.p.b,"pb"),n(c.p.c,"pc"),m("label.v","Pago"),l(c.v.u),l(c.v.b),l(c.v.c),m("label","Vuelto"),m(""),m(""),m(""),m(".t","."),n(c.d.u,"du"),n(c.d.b,"db"),n(c.d.c,"dc"),m("label","Devol"),l(c.c.u),l(c.c.b),l(c.c.c),m("label.v","Cierre"),m(""),m(""),m(""),m(".t","."),n(c.e.u,"eu"),n(c.e.b,"eb"),n(c.e.c,"ec")]))}}const styl=b=>b<0?"background:tomato":"",inputfn=b=>(c,u)=>m("input.item",{type:"number",style:styl(c),value:c,id:u,onchange:b}),num=b=>""==b?0:parseFloat(b)
+function AppView(){const b={u2b:200,c2b:50}
+function c(c){let{value:u,id:t}=c.target
+u=num(u),b[t]=u}const u=(b,u,t)=>m(".itex",[m("label.h",b),inputfn(c)(u,t)])
+return{view:c=>m("",[m(".flx",[u("Bs/$",b.u2b,"u2b"),u("Bs/Co",b.c2b,"c2b")]),m(G,{...b})])}}m.mount(document.getElementById("app"),AppView)
