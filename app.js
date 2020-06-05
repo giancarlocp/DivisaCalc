30d6a30cc6d3f1e51b7bcbea4fd4ce94f05f8768
Giancarlos <giancarlocp@yahoo.com>
2020-06-04 21:20:24 -0400

    more FP, improve UX

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 navigator.serviceWorker&&navigator.serviceWorker.register("sw.js")
-const pr=1e5,fx=b=>Math.round(b*pr)/pr
-function G({attrs:b}){console.log("G",b)
-const c={f:{u:5,b:1e3,c:20},p:{u:0,b:0,c:0},v:{u:0,b:0,c:0},d:{u:0,b:0,c:0},c:{u:0,b:0,c:0},e:{u:0,b:0,c:0}}
-function u(b,u){"u"===u?(c[b].b=fx(c[b].u*u2b),c[b].c=fx(c[b].b/c2b)):"b"===u?(c[b].u=fx(c[b].b/u2b),c[b].c=fx(c[b].b/c2b)):"c"===u&&(c[b].b=fx(c[b].c*c2b),c[b].u=fx(c[b].b/u2b))}const t=b=>c[b].u*u2b+c[b].b+c[b].c*c2b
-function e(b){let[u,e,n]="d"==b?"vdc":"fpv"
-c[n].b=fx(t(e)-c[u].b),c[n].u=fx(c[n].b/u2b),c[n].c=fx(c[n].b/c2b)}const n=inputfn(function(b){let{value:t,id:n}=b.target
-t=num(t)
-const[l,m]=n
-c[l][m]=t,"f"==l?(u(l,m),e(l)):"p"==l||"d"==l?e(l):"e"==l&&u(l,m)}),l=b=>m(".item",{style:styl(b)},b)
-return{view:({attrs:b})=>(u2b=b.u2b,c2b=b.c2b,m(".container",[m("label","$"),m("label","Bs"),m("label","Co"),m(""),n(c.f.u,"fu"),n(c.f.b,"fb"),n(c.f.c,"fc"),m("label.v","Fact"),n(c.p.u,"pu"),n(c.p.b,"pb"),n(c.p.c,"pc"),m("label.v","Pago"),l(c.v.u),l(c.v.b),l(c.v.c),m("label","Vuelto"),m(""),m(""),m(""),m(".t","."),n(c.d.u,"du"),n(c.d.b,"db"),n(c.d.c,"dc"),m("label","Devol"),l(c.c.u),l(c.c.b),l(c.c.c),m("label.v","Cierre"),m(""),m(""),m(""),m(".t","."),n(c.e.u,"eu"),n(c.e.b,"eb"),n(c.e.c,"ec")]))}}const styl=b=>b<0?"background:tomato":"",inputfn=b=>(c,u)=>m("input.item",{type:"number",style:styl(c),value:c,id:u,onchange:b}),num=b=>""==b?0:parseFloat(b)
-function AppView(){const b={u2b:200,c2b:50}
-function c(c){let{value:u,id:t}=c.target
-u=num(u),b[t]=u}const u=(b,u,t)=>m(".itex",[m("label.h",b),inputfn(c)(u,t)])
-return{view:c=>m("",[m(".flx",[u("Bs/$",b.u2b,"u2b"),u("Bs/Co",b.c2b,"c2b")]),m(G,{...b})])}}m.mount(document.getElementById("app"),AppView)
+function mapObj(e,t){let n={};for(let u in t)n[u]=e(t[u],u,t);return n}
+let fx=e=>Math.round(1e5*e)/1e5,conv=(e,t,n)=>(u,o)=>t==o?n:fx(e[t+o](n)),num=e=>""==e?0:parseFloat(e),styl=e=>e<0?"background:tomato":"",noedit=e=>m(".item",{style:styl(e)},e),input_=e=>(t,n)=>m("input.item",{type:"number",style:styl(t),value:t,id:n,oninput:e}),noRow=({u:e,b:t,c:n},u,o)=>[noedit(e),noedit(t),noedit(n),m("label",o)],row_=e=>({u:t,b:n,c:u},o,c)=>[e(t,o+"u"),e(n,o+"b"),e(u,o+"c"),m(c?"label.v":"",c)]
+function G(){let e,t,n={r:"u",f:{u:5,b:1e3,c:20},p:{u:0,b:0,c:0},v:{u:0,b:0,c:0},d:{u:0,b:0,c:0},c:{u:0,b:0,c:0},e:{u:0,b:0,c:0}},u={ub:t=>t*e,bu:t=>t/e,uc:n=>n*e/t,cb:e=>e*t,bc:e=>e/t,cu:n=>n*t/e}
+function o(e,t,o){n[e]=mapObj(conv(u,t,o),n[e])}let c=({u:n,b:u,c:o})=>n*e+u+o*t
+function i(e){let[t,o,i]=e,l=c(n[o])-n[t].b;n[i]=mapObj(conv(u,"b",l),n[i])}let l=()=>{i("fpv"),i("vdc")},b=(e,t)=>{o("f",e,t),l(),n.r=e}
+function r({target:{value:e,id:t}}){e=num(e);let[u,c]=t;n[u][c]=e,"f"==u?b(c,e):"e"==u?o(u,c,e):l()}input_(r);let a=row_(input_(r))
+return{view:({attrs:u})=>(e=u.u2b,t=u.c2b,b(n.r,n.f[n.r]),m(".container",[m("label","$"),m("label","Bs"),m("label","Co"),m(""),a(n.f,"f","Fact"),a(n.p,"p","Pago"),noRow(n.v,"v","Vuelto"),hideRow,a(n.d,"d","Devol"),noRow(n.c,"c","Cierre"),hideRow,a(n.e,"e")]))}}
+function App(){let e={u2b:200,c2b:50}
+function t({target:{value:t,id:n}}){t=num(t),e[n]=t}let n=(e,n,u)=>m(".itex",[m("label.h",e),input_(t)(n,u)])
+return{view:t=>m("",[m(".flx",[n("Bs/$",e.u2b,"u2b"),n("Bs/Co",e.c2b,"c2b")]),m(G,{...e})])}}hideRow=[m(""),m(""),m(""),m(".t",".")],m.mount(document.getElementById("app"),App)
--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-let ael=self.addEventListener,ver="1"
-ael("install",e=>e.waitUntil(caches.open(ver).then(e=>e.addAll(["/","/a.js","/s.css","/favicon.png","https://pagecdn.io/lib/mithril/2.0.4/mithril.min.js"])).catch(console.log))),ael("activate",e=>{e.waitUntil(caches.keys().then(e=>e.forEach(e=>{e!=ver&&caches.delete(e)})))})
-let cama=e=>caches.match(e),capu=(e,t,a)=>caches.open(e).then(e=>e.put(t,a))
-ael("fetch",e=>{let t=e.request,a=cama(t).then(e=>e||fetch(t).then(e=>(capu(ver,t,e),e.clone()))).catch(console.log)
-e.respondWith(a)})
+let ael=self.addEventListener
+let ver='2'
+ael('install',ev=>ev.waitUntil(caches.open(ver).then(cache=>cache.addAll(['/','/app.js','/s.css','/favicon.png','/mithril.min.js',])).catch(console.log)))
+ael('activate',ev=>{ev.waitUntil(caches.keys().then(ks=>ks.forEach(k=>{if(k!=ver)caches.delete(k)})))})
+let cama=req=>caches.match(req)
+let capu=(ver,req,res)=>caches.open(ver).then(cache=>cache.put(req,res))
+ael('fetch',ev=>{let req=ev.request
+let resCa=cama(req).then(res=>res?res:fetch(req).then(newRes=>{capu(ver,req,newRes)
+return newRes.clone()})).catch(console.log);ev.respondWith(resCa)})
