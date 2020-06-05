37e2651f5aad051a23252b351ca0f3e072ccb2b3
Giancarlos <giancarlocp@yahoo.com>
2020-06-05 18:01:25 -0400

    change strategy to Network with Cache Fallback

--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
-let ael=self.addEventListener
-let ver='2'
-ael('install',ev=>ev.waitUntil(caches.open(ver).then(cache=>cache.addAll(['/','/app.js','/s.css','/favicon.png','/mithril.min.js',])).catch(console.log)))
-ael('activate',ev=>{ev.waitUntil(caches.keys().then(ks=>ks.forEach(k=>{if(k!=ver)caches.delete(k)})))})
-let cama=req=>caches.match(req)
-let capu=(ver,req,res)=>caches.open(ver).then(cache=>cache.put(req,res))
-ael('fetch',ev=>{let req=ev.request
-let resCa=cama(req).then(res=>res?res:fetch(req).then(newRes=>{capu(ver,req,newRes)
-return newRes.clone()})).catch(console.log);ev.respondWith(resCa)})
+let ael=self.addEventListener,ver="3"
+ael("install",e=>e.waitUntil(caches.open(ver).then(e=>e.addAll(["/","/app.js","/s.css","/favicon.png","/mithril.min.js"])).catch(console.log))),ael("activate",e=>{e.waitUntil(caches.keys().then(e=>e.forEach(e=>{e!=ver&&caches.delete(e)})))})
+let cama=e=>caches.match(e),capu=(e,a,c)=>caches.open(e).then(e=>e.put(a,c))
+ael("fetch",e=>{let a=e.request,c=fetch(a).then(e=>e?(capu(ver,a,e),e.clone()):cama(a)).catch(e=>cama(a));e.respondWith(c)})
