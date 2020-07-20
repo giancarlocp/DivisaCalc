2666e0fb1b77a71ca6bfd3eee4a47216676f6e1b
Giancarlos <giancarlocp@yahoo.com>
2020-07-20 15:50:15 -0400

    change sw strategy to "Cache with Network Update". Add localStorage

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 navigator.serviceWorker&&navigator.serviceWorker.register("sw.js")
-function mapObj(e,t){let n={};for(let u in t)n[u]=e(t[u],u,t);return n}
-let fx=e=>Math.round(1e5*e)/1e5,conv=(e,t,n)=>(u,o)=>t==o?n:fx(e[t+o](n)),num=e=>""==e?0:parseFloat(e),styl=e=>e<0?"background:tomato":"",noedit=e=>m(".item",{style:styl(e)},e),input_=e=>(t,n)=>m("input.item[type=number]",{min:0,style:styl(t),value:t,id:n,oninput:e}),noRow=({u:e,b:t,c:n},u,o)=>[noedit(e),noedit(t),noedit(n),m("label",o)],row_=e=>({u:t,b:n,c:u},o,c)=>[e(t,o+"u"),e(n,o+"b"),e(u,o+"c"),m(c?"label.v":"",c)]
-function G(){let e,t,n={r:"u",f:{u:5,b:1e3,c:20},p:{u:0,b:0,c:0},v:{u:0,b:0,c:0},d:{u:0,b:0,c:0},c:{u:0,b:0,c:0},e:{u:0,b:0,c:0}},u={ub:t=>t*e,bu:t=>t/e,uc:n=>n*e/t,cb:e=>e*t,bc:e=>e/t,cu:n=>n*t/e}
-function o(e,t,o){n[e]=mapObj(conv(u,t,o),n[e])}let c=({u:n,b:u,c:o})=>n*e+u+o*t
-function i(e){let[t,o,i]=e,l=c(n[o])-n[t].b;n[i]=mapObj(conv(u,"b",l),n[i])}let l=()=>{i("fpv"),i("vdc")},b=(e,t)=>{o("f",e,t),l(),n.r=e}
-function r({target:{value:e,id:t}}){e=(e=num(e))<0?-1*e:e;let[u,c]=t;n[u][c]=e,"f"==u?b(c,e):"e"==u?o(u,c,e):l()}input_(r);let a=row_(input_(r))
-return{view:({attrs:u})=>(e=u.u2b,t=u.c2b,b(n.r,n.f[n.r]),m(".container",[m("label","$"),m("label","Bs"),m("label","Co"),m(""),a(n.f,"f","Fact"),a(n.p,"p","Pago"),noRow(n.v,"v","Vuelto"),hideRow,a(n.d,"d","Devol"),noRow(n.c,"c","Cierre"),hideRow,a(n.e,"e")]))}}function App(){let e={u2b:200,c2b:50}
-function t({target:{value:t,id:n}}){t=num(t),e[n]=t}let n=(e,n,u)=>m(".itex",[m("label.h",e),input_(t)(n,u)])
-return{view:t=>m("",[m(".flx",[n("Bs/$",e.u2b,"u2b"),n("Bs/Co",e.c2b,"c2b")]),m(G,{...e})])}}hideRow=[m(""),m(""),m(""),m(".t",".")],m.mount(document.getElementById("app"),App)
+function mapObj(fn,obj){let nObj={}
+for(let k in obj)
+nObj[k]=fn(obj[k],k,obj)
+return nObj}
+let fx=n=>Math.round(n*1e5)/1e5
+let conv=(f,u,m)=>(v,k)=>u==k?m:fx(f[u+k](m))
+let num=v=>''==v?0:parseFloat(v)
+let styl=(v)=>v<0?'background:tomato':''
+let noedit=(v)=>m('.item',{style:styl(v)},v)
+let input_=(oninput)=>(value,id)=>m('input.item[type=number][min=0]',{style:styl(value),value,id,oninput})
+let noRow=({u,b,c},r,l)=>[noedit(u),noedit(b),noedit(c),m('label',l),]
+let row_=(input)=>({u,b,c},r,l)=>[input(u,r+'u'),input(b,r+'b'),input(c,r+'c'),m(l?'label.v':'',l),]
+hideRow=[m(''),m(''),m(''),m('.t','.')]
+function G(){let u2b,c2b
+let d={r:'u',f:{u:5,b:0,c:0},p:{u:0,b:0,c:0},v:{u:0,b:0,c:0},d:{u:0,b:0,c:0},c:{u:0,b:0,c:0},e:{u:0,b:0,c:0},}
+let F={ub:u=>u*u2b,bu:b=>b/u2b,uc:u=>u*u2b/c2b,cb:c=>c*c2b,bc:b=>b/c2b,cu:c=>c*c2b/u2b,}
+function cost(r,c,m){d[r]=mapObj(conv(F,c,m),d[r])}
+let t=({u,b,c})=>u*u2b+b+c*c2b
+function pago(z){let[j,k,l]=z
+let v=fx(t(d[k])-d[j].b)
+d[l]=mapObj(conv(F,'b',v),d[l])}
+let pag=()=>{pago('fpv');pago('vdc')}
+let all=(c,v)=>{cost('f',c,v);pag();d.r=c}
+function change({target:{value,id}}){value=num(value)
+value=value<0?-1*value:value
+let[r,c]=id
+d[r][c]=value
+if(r=='f')
+all(c,value)
+else if(r=='e')
+cost(r,c,value)
+else
+pag()}
+let input=input_(change)
+let inRow=row_(input_(change))
+return{view({attrs:s}){u2b=s.u2b
+c2b=s.c2b
+all(d.r,d.f[d.r])
+return m('.container',[m('label','$'),m('label','Bs'),m('label','Co'),m(''),inRow(d.f,'f','Fact'),inRow(d.p,'p','Pago'),noRow(d.v,'v','Vuelto'),hideRow,inRow(d.d,'d','Devol'),noRow(d.c,'c','Cierre'),hideRow,inRow(d.e,'e'),])}}}
+let LocalStore={get:(k)=>JSON.parse(localStorage.getItem(k)),set(k,state){localStorage.setItem(k,JSON.stringify(state))}}
+function App(){let s={u2b:200,c2b:50}
+Object.assign(s,LocalStore.get('s'))
+function change({target:{value,id}}){value=num(value)
+s[id]=value
+LocalStore.set('s',s)}
+let input=(label,val,id)=>m('.itex',[m('label.h',label),input_(change)(val,id)])
+return{view:(vnode)=>m('',[m('.flx',[input('Bs/$',s.u2b,'u2b'),input('Bs/Co',s.c2b,'c2b'),]),m(G,{...s}),])}}
+m.mount(document.getElementById('app'),App)
--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
 let ael=self.addEventListener,ver="3"
-ael("install",e=>e.waitUntil(caches.open(ver).then(e=>e.addAll(["/","/app.js","/s.css","/favicon.png","/mithril.min.js"])).catch(console.log))),ael("activate",e=>{e.waitUntil(caches.keys().then(e=>e.forEach(e=>{e!=ver&&caches.delete(e)})))})
-let cama=e=>caches.match(e),capu=(e,a,c)=>caches.open(e).then(e=>e.put(a,c))
-ael("fetch",e=>{let a=e.request,c=fetch(a).then(e=>e?(capu(ver,a,e),e.clone()):cama(a)).catch(e=>cama(a));e.respondWith(c)})
+ael('install',ev=>ev.waitUntil(caches.open(ver).then(cache=>cache.addAll(['/','/s.css','/app.js','/favicon.png','https://pagecdn.io/lib/mithril/2.0.4/mithril.min.js',])).catch(console.log)))
+ael('activate',ev=>{ev.waitUntil(caches.keys().then(ks=>ks.forEach(k=>{k!=ver&&caches.delete(k)})))})
+let cama=req=>caches.match(req)
+let caup=(ver,req)=>caches.open(ver).then(cache=>{fetch(req).then(res=>cache.put(req,res))
+return cama(req)})
+ael('fetch',ev=>{let req=ev.request
+let resCa=caup(ver,req).catch(console.log)
+ev.respondWith(resCa)})
